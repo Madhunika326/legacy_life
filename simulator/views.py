c9a873a33f784e525e30e8a553f147f0d3bdd78f
Madhunika K J <madhunikakj326@gmail.com>
2025-07-28 23:43:39 +0530

    Added basic Donation Simulator feature and linked it on Home page

--- simulator/views.py
+++ simulator/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 def simulator_home(request):
     if request.method == 'POST':
-        organs = request.POST.getlist('organs')
-        lives_saved = len(organs) * 3  # Pedicted logic : each organ can help 3 people
+        organs = request.POST.getlist('organs')  # Get organs from the form
+        lives_saved = len(organs) * 3  #  Simple logic: each organ helps 3 people
         return render(request, 'simulator_result.html', {
             'organs': organs,
             'lives_saved': lives_saved
         })
+    
+    #  If not POST, show the simulator form
     return render(request, 'simulator.html')
-
